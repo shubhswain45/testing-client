fe73bdfd4a6fd3382383b5988cc4bee1a8426972
shubhswain45 <shubhkantswain@.com>
2024-11-10 18:54:14 +0530

    some bg colors

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
@@ -6,10 +6,10 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("animate-pulse rounded-md bg-muted dark:bg-neutral-800", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
