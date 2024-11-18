d58fcd16eca442dde63065b48f1cded2f8a7dd59
shubhswain45 <shubhkantswain@.com>
2024-11-18 16:55:27 +0530

    fixing params error

--- app/dashboard/[username]/page.tsx
+++ app/dashboard/[username]/page.tsx
@@ -2,18 +2,19 @@ import PostsGrid from "@/components/PostsGrid";
 import { getUserPosts } from "@/lib/data";
 import type { Post } from "@/gql/graphql";
 
-interface ProfilePageProps {
-  params: {
-    username: string;
-  };
-}
-
-async function ProfilePage({ params }: ProfilePageProps) {
+// The params object is passed as an argument to the ProfilePage component.
+async function ProfilePage({
+  params,
+}: {
+  params: { username: string }; // Typing params directly
+}) {
+  // Fetch the posts for the given user
   const posts = await getUserPosts(params.username);
 
   // Filter out null values
   const filteredPosts = posts?.filter((post): post is Post => post !== null);
 
+  // Render the PostsGrid with the filtered posts
   return <PostsGrid posts={filteredPosts} />;
 }
 
