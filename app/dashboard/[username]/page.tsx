87e3e39aa867d5e92b7384cc0bafb9b310f4150b
shubhswain45 <shubhkantswain@.com>
2024-11-18 16:50:55 +0530

    some changes

--- app/dashboard/[username]/page.tsx
+++ app/dashboard/[username]/page.tsx
@@ -2,12 +2,14 @@ import PostsGrid from "@/components/PostsGrid";
 import { getUserPosts } from "@/lib/data";
 import type { Post } from "@/gql/graphql";
 
-async function ProfilePage({
-  params: { username },
-}: {
-  params: { username: string };
-}) {
-  const posts = await getUserPosts(username);
+interface ProfilePageProps {
+  params: {
+    username: string;
+  };
+}
+
+async function ProfilePage({ params }: ProfilePageProps) {
+  const posts = await getUserPosts(params.username);
 
   // Filter out null values
   const filteredPosts = posts?.filter((post): post is Post => post !== null);
